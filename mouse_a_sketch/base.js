14474b349c40f062d102cf57bc67053e5ab69889
Marcel Degas <marcel.degas@gmail.com>
2015-08-21 13:04:45 -0700

    base changes

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -2,4 +2,14 @@ console.log("this is a sanity check");
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+  var $color = $("input")
+  activeColor = $color.val()
+  
+//  $color.on("change", function() {
+//    activeColor = $color.val();
+//  }
+  
+  $("td").on("mouseover", function(event) {
+    $(this).css("background-color", activeColor);
+    });
 })
